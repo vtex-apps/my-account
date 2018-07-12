11127b7f131b2aa8984521ae741e1ca798b6531c
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-12 10:47:25 -0300

    Switch boolean value

--- react/pages/PersonalData.js
+++ react/pages/PersonalData.js
@@ -10,7 +10,7 @@ class PersonalData extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isEditingData: true,
+      isEditingData: false,
       isEditingPassword: false,
     }
   }
