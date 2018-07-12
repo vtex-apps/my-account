efe326c5eaab15fc287c0dc0826b25e397de664e
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-12 17:13:35 -0300

    Refactor ContentBox

--- react/components/shared/ContentBox.js
+++ react/components/shared/ContentBox.js
@@ -2,28 +2,50 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ClearButton from './ClearButton'
 
-const ContentBox = ({ children, width, lowerButton, onLowerButtonClick }) => {
-  const widthClass = width ? 'w-' + width + '-ns' : null
-
+const ContentBox = ({
+  children,
+  width,
+  header,
+  lowerButton,
+  onLowerButtonClick,
+  upperButton,
+  onUpperButtonClick,
+}) => {
+  const widthClass = width && `w-${width}-ns`
   return (
-    <div
-      className={'ba bw1 b--light-gray br2 ph7 pv6 mb6 mr5-ns ' + widthClass}
-    >
-      {children}
-      {lowerButton && (
-        <footer className="flex justify-end mt6">
-          <ClearButton onClick={onLowerButtonClick}>{lowerButton}</ClearButton>
-        </footer>
-      )}
+    <div className={'pb5 pr5-ns ' + widthClass}>
+      <article className={'ba bw1 b--light-gray br2'}>
+        {header && (
+          <header className="bg-black-10 pv5 f7 ph7 black-90">{header}</header>
+        )}
+        {upperButton && (
+          <header className="flex justify-end pt5 ph7">
+            <ClearButton onClick={onUpperButtonClick}>
+              {upperButton}
+            </ClearButton>
+          </header>
+        )}
+        <main className="ph7 pv5">{children}</main>
+        {lowerButton && (
+          <footer className="flex justify-end ph7 pb5">
+            <ClearButton onClick={onLowerButtonClick}>
+              {lowerButton}
+            </ClearButton>
+          </footer>
+        )}
+      </article>
     </div>
   )
 }
 
 ContentBox.propTypes = {
   children: PropTypes.any,
+  header: PropTypes.string,
   width: PropTypes.number,
   lowerButton: PropTypes.string,
   onLowerButtonClick: PropTypes.func,
+  upperButton: PropTypes.string,
+  onUpperButtonClick: PropTypes.func,
 }
 
 export default ContentBox
