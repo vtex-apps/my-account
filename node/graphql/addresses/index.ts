87808216d08e164d236b68597f3c8c6f57462314
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-13 14:18:14 -0300

    Refactor resolver structure

---
+++ node/graphql/addresses/index.ts
@@ -0,0 +1,80 @@
+/* Mocked-up database */
+const addresses = [
+  {
+    addressId: '10',
+    addressType: 'residential',
+    city: 'São Paulo',
+    complement: 'ap. 102',
+    country: 'BRA',
+    geoCoordinates: [],
+    neighborhood: 'Penha',
+    number: '788',
+    postalCode: '03612-000',
+    receiverName: 'Gustavo Silva',
+    reference: null,
+    state: 'SP',
+    street: 'R. Coronel Meireles',
+  },
+  {
+    addressId: '10',
+    addressType: 'residential',
+    city: 'Rio de Janeiro',
+    complement: '',
+    country: 'BRA',
+    geoCoordinates: [],
+    neighborhood: 'Flamengo',
+    number: '96',
+    postalCode: '22501-080',
+    receiverName: 'Gustavo Silva',
+    reference: null,
+    state: 'SP',
+    street: 'Av. Praia de Botafogo',
+  },
+  {
+    addressId: '10',
+    addressType: 'residential',
+    city: 'Cupertino',
+    complement: '',
+    country: 'USA',
+    geoCoordinates: [],
+    neighborhood: '',
+    number: '1',
+    postalCode: '95014',
+    receiverName: 'Jony Ive',
+    reference: null,
+    state: 'CA',
+    street: 'Infinite Loop',
+  },
+]
+
+export const Query = {
+  addresses: (_, args, ctx) => {
+    console.log('Pinged [addresses]')
+    return addresses
+  },
+}
+
+export const Mutation = {}
+
+// export const resolvers = {
+//   Mutation: {
+//     addCar: (_, args, ctx) => {
+//       console.log('[addCar] Received vtex context:', ctx.vtex)
+//       console.log('[addCar] Received arguments:', args)
+//       return cars[0]
+//     },
+//   },
+//   Query: {
+//     allCars: (_, args, ctx) => {
+//       console.log('[allCars] Received vtex context:', ctx.vtex)
+//       console.log('[allCars] Received arguments:', args)
+//       return cars
+//     },
+
+//     carsByYear: (_, args, ctx) => {
+//       console.log('[carsByYear] Received vtex context:', ctx.vtex)
+//       console.log('[carsByYear] Received arguments:', args)
+//       return cars.filter(car => car.year == args.year)
+//     },
+//   },
+// }
--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,50 +1,11 @@
-/**
- *  Simple in-memory database
- */
-const cars = [
-  {
-    model: 'Fiesta',
-    year: 2010,
-    maker: 'Ford',
-  },
-  {
-    model: 'Uno',
-    year: 1972,
-    maker: 'Fiat',
-  },
-  {
-    model: 'Corsa',
-    year: 2010,
-    maker: 'Chevrolet',
-  },
-]
+import { Query as AddressQuery } from './addresses'
+import { Mutation as AddressMutation } from './addresses'
 
-/**
- * You shoud write your GraphQL resolvers in here.
- * Resolvers are just usual node functions, so here you can make
- * API calls, connect to the database and transform the data in
- * any way you wish. You can learn more about resolvers and its arguments
- * here: https://www.apollographql.com/docs/graphql-tools/resolvers.html
- */
 export const resolvers = {
   Mutation: {
-    addCar: (_, args, ctx) => {
-      console.log('[addCar] Received vtex context:', ctx.vtex)
-      console.log('[addCar] Received arguments:', args)
-      return cars[0]
-    },
+    ...AddressMutation,
   },
   Query: {
-    allCars: (_, args, ctx) => {
-      console.log('[allCars] Received vtex context:', ctx.vtex)
-      console.log('[allCars] Received arguments:', args)
-      return cars
-    },
-
-    carsByYear: (_, args, ctx) => {
-      console.log('[carsByYear] Received vtex context:', ctx.vtex)
-      console.log('[carsByYear] Received arguments:', args)
-      return cars.filter(car => car.year == args.year)
-    },
+    ...AddressQuery,
   },
 }
