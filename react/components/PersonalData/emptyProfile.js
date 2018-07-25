86ae39a05eb45c7e604f16de943b797e556251a5
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-25 16:41:09 -0300

    Add placeholder profile

---
+++ react/components/PersonalData/emptyProfile.js
@@ -0,0 +1,14 @@
+export default {
+  birthDate: null,
+  businessPhone: null,
+  corporateDocument: null,
+  corporateName: null,
+  document: null,
+  email: null,
+  firstName: null,
+  gender: null,
+  homePhone: null,
+  lastName: null,
+  stateRegistration: null,
+  tradeName: null,
+}
