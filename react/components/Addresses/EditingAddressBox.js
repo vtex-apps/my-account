889681f367d78e98aec93c826eb7fb3b40dc458b
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-26 11:07:42 -0300

    Link button to mutation

--- react/components/Addresses/EditingAddressBox.js
+++ react/components/Addresses/EditingAddressBox.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
+import { graphql } from 'react-apollo'
+import { compose } from 'recompose'
 import Button from '@vtex/styleguide/lib/Button'
 import Input from '@vtex/styleguide/lib/Input'
 import ContentBox from '../shared/ContentBox'
+import DeleteAddress from '../../graphql/DeleteAddress.gql'
+import AddressShape from '../../node_modules/@vtex/address-form/lib/propTypes/AddressShape'
 
 class EditingAddressBox extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isDeleting: false,
+    }
+  }
+
+  onDeleteClick = () => {
+    if (this.state.isDeleting) return
+
+    this.setState({
+      isDeleting: true,
+    })
+    this.props
+      .deleteAddress({
+        variables: {
+          addressId: this.props.address.addressId,
+        },
+      })
+      .then(this.props.onAddressDeleted)
+  }
+
   render() {
     const { isNew, intl } = this.props
+    const { isDeleting } = this.state
     const intlId = isNew ? 'addresses.addAddress' : 'addresses.saveAddress'
     return (
       <ContentBox width={'third'}>
@@ -20,7 +48,14 @@ class EditingAddressBox extends Component {
           </Button>
           {!isNew && (
             <div className="mt5">
-              <Button type="button" variation="danger" block size="small">
+              <Button
+                type="button"
+                variation="danger"
+                block
+                size="small"
+                onClick={this.onDeleteClick}
+                isLoading={isDeleting}
+              >
                 {intl.formatMessage({ id: 'addresses.deleteAddress' })}
               </Button>
             </div>
@@ -33,7 +68,21 @@ class EditingAddressBox extends Component {
 
 EditingAddressBox.propTypes = {
   isNew: PropTypes.bool,
+  onAddressDeleted: PropTypes.func,
+  address: AddressShape,
   intl: intlShape.isRequired,
 }
 
-export default injectIntl(EditingAddressBox)
+const deleteAddressMutation = {
+  name: 'deleteAddress',
+  options({ addressId }) {
+    return {
+      variables: { addressId },
+    }
+  },
+}
+const enhance = compose(
+  graphql(DeleteAddress, deleteAddressMutation),
+  injectIntl,
+)
+export default enhance(EditingAddressBox)
