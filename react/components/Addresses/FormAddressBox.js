336a176cb918b1c03da078052972bcbac882aea7
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-07-27 11:26:04 -0300

    Finish joining files from last feature

--- react/components/Addresses/FormAddressBox.js
+++ react/components/Addresses/FormAddressBox.js
@@ -1,66 +1,124 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
-import { graphql } from 'react-apollo'
-import { compose } from 'recompose'
-import Button from '@vtex/styleguide/lib/Button'
-import Input from '@vtex/styleguide/lib/Input'
+import { Button } from 'vtex.styleguide'
+import {
+  addValidation,
+  AddressContainer,
+  CountrySelector,
+  PostalCodeGetter,
+  AddressForm,
+  AutoCompletedFields,
+  AddressRules,
+  AddressSubmitter,
+} from '@vtex/address-form'
+import StyleguideInput from '@vtex/address-form/lib/inputs/StyleguideInput'
+import AddressShape from '@vtex/address-form/lib/propTypes/AddressShape'
 import ContentBox from '../shared/ContentBox'
-import DeleteAddress from '../../graphql/DeleteAddress.gql'
-import AddressShape from '../../node_modules/@vtex/address-form/lib/propTypes/AddressShape'
+import emptyAddress from './emptyAddress'
+import AddressDeletter from './AddressDeletter'
 
 class FormAddressBox extends Component {
   constructor(props) {
     super(props)
-
+    const baseAddress = this.props.isNew ? emptyAddress : this.props.address
     this.state = {
-      isDeleting: false,
+      address: this.prepareAddress(baseAddress),
+      shipsTo: this.getShippingCountries(),
     }
   }
 
-  onDeleteClick = () => {
-    if (this.state.isDeleting) return
+  prepareAddress(address) {
+    const { __typename, ...addr } = address
+    return addValidation(addr)
+  }
 
-    this.setState({
-      isDeleting: true,
-    })
-    this.props
-      .deleteAddress({
-        variables: {
-          addressId: this.props.address.addressId,
-        },
-      })
-      .then(this.props.onAddressDeleted)
+  getShippingCountries() {
+    const { intl } = this.props
+
+    return [
+      'ARG',
+      'BOL',
+      'BRA',
+      'CAN',
+      'CHL',
+      'COL',
+      'ECU',
+      'ESP',
+      'GTM',
+      'MEX',
+      'PER',
+      'PRT',
+      'PRY',
+      'URY',
+      'USA',
+      'VEN',
+    ].map(countryCode => ({
+      label: intl.formatMessage({ id: 'country.' + countryCode }),
+      value: countryCode,
+    }))
+  }
+
+  handleAddressChange = newAddress => {
+    this.setState(prevState => ({
+      address: {
+        ...prevState.address,
+        ...newAddress,
+      },
+    }))
+  }
+
+  handleSubmit = (valid, address) => {
+    console.log('field is being submitted')
+    console.log(valid)
+    console.log(address)
   }
 
   render() {
-    const { isNew, intl } = this.props
-    const { isDeleting } = this.state
+    const { onAddressDeleted, isNew, intl } = this.props
+    const { address, shipsTo } = this.state
+
     const intlId = isNew ? 'addresses.addAddress' : 'addresses.saveAddress'
     return (
       <ContentBox width={'third'}>
-        <form>
-          <div className="mb7">
-            <Input name="address" label="Endereço" />
-          </div>
-          <Button type="submit" variation="secondary" block size="small">
-            {intl.formatMessage({ id: intlId })}
-          </Button>
-          {!isNew && (
-            <div className="mt5">
-              <Button
-                type="button"
-                variation="danger"
-                block
-                size="small"
-                onClick={this.onDeleteClick}
-                isLoading={isDeleting}
-              >
-                {intl.formatMessage({ id: 'addresses.deleteAddress' })}
-              </Button>
+        <AddressRules
+          country={address.country.value}
+          fetch={country => import('@vtex/address-form/lib/country/' + country)}
+        >
+          <AddressContainer
+            address={address}
+            onChangeAddress={this.handleAddressChange}
+            Input={StyleguideInput}
+            autoCompletePostalCode={true}
+          >
+            <div>
+              <CountrySelector shipsTo={shipsTo} />
+              <PostalCodeGetter />
+              <AutoCompletedFields>
+                <a>{intl.formatMessage({ id: 'address-form.edit' })}</a>
+              </AutoCompletedFields>
+              <AddressForm />
+              <AddressSubmitter onSubmit={this.handleSubmit}>
+                {handleSubmit => (
+                  <Button
+                    onClick={handleSubmit}
+                    variation="secondary"
+                    block
+                    size="small"
+                  >
+                    {intl.formatMessage({ id: intlId })}
+                  </Button>
+                )}
+              </AddressSubmitter>
             </div>
-          )}
-        </form>
+          </AddressContainer>
+        </AddressRules>
+        {!isNew && (
+          <AddressDeletter
+            addressId={address.addressId.value}
+            onAddressDeleted={onAddressDeleted}
+          />
+        )}
       </ContentBox>
     )
   }
@@ -73,16 +131,4 @@ FormAddressBox.propTypes = {
   intl: intlShape.isRequired,
 }
 
-const deleteAddressMutation = {
-  name: 'deleteAddress',
-  options({ addressId }) {
-    return {
-      variables: { addressId },
-    }
-  },
-}
-const enhance = compose(
-  graphql(DeleteAddress, deleteAddressMutation),
-  injectIntl,
-)
-export default enhance(FormAddressBox)
+export default injectIntl(FormAddressBox)
