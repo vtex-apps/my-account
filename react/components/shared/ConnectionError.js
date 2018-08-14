f053800d8484d7bff63d20ab20ae1e0e295bc913
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-14 14:24:46 -0300

    Change error alert prop

--- react/components/shared/ConnectionError.js
+++ react/components/shared/ConnectionError.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
 import { Alert } from 'vtex.styleguide'
 
-const ConnectionError = ({ intl }) => {
+const ConnectionError = ({ onReload, intl }) => {
   return (
     <div className="mt7">
       <Alert
         type="error"
         action={{
           label: intl.formatMessage({ id: 'error.reload' }),
-          onClick: () => console.log('Went back!'),
+          onClick: onReload,
         }}
       >
         {intl.formatMessage({ id: 'error.connection' })}
@@ -20,6 +20,7 @@ const ConnectionError = ({ intl }) => {
 }
 
 ConnectionError.propTypes = {
+  onReload: PropTypes.func.isRequired,
   intl: intlShape.isRequired,
 }
 
