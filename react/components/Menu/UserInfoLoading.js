e24eb8b3f9755f41ddc7318def540d99f4821d54
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-14 16:46:01 -0300

    Add user greeting skeleton

---
+++ react/components/Menu/UserInfoLoading.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import UserPlaceholderPicture from './UserPlaceholderPicture'
+import SkeletonPiece from '../shared/SkeletonPiece'
+
+const UserInfoLoading = () => {
+  return (
+    <div className="flex items-end mb7">
+      <div className="mr5 relative">
+        <div className="h3 w3 br-100 bg-light-silver">
+          <div className="shimmer" />
+        </div>
+      </div>
+      <div className="flex-auto">
+        <div className="mb2">
+          <SkeletonPiece width={50} />
+        </div>
+        <div>
+          <SkeletonPiece />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserInfoLoading
