f65448a39042202f505958c2cb06fd7d0829ceb9
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-15 14:59:13 -0300

    Finish mock cards list

---
+++ react/components/Payments/PaymentBox.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { injectIntl, intlShape } from 'react-intl'
+import { AddressSummary, AddressRules } from '@vtex/address-form'
+import Elo from './flags/Elo'
+import ContentBox from '../shared/ContentBox'
+import DataEntry from '../shared/DataEntry'
+
+const PaymentBox = ({ intl, address }) => {
+  return (
+    <ContentBox lowerButton={intl.formatMessage({ id: 'commons.edit' })}>
+      <div className="w5">
+        <div className="mb7">
+          <DataEntry label={'Titular'}>
+            {'Gustavo Henrique Faustino Silva'}
+          </DataEntry>
+        </div>
+        <div className="mb7">
+          <DataEntry>
+            <div className="flex items-center">
+              <div className="h2 mr4">
+                <Elo />
+              </div>
+              <span>**** **** **** 7462</span>
+            </div>
+          </DataEntry>
+        </div>
+        <div className="mb7">
+          <DataEntry label={'Vencimento'}>{'10/2020'}</DataEntry>
+        </div>
+        <DataEntry label={'Faturamento'}>
+          <div className="lh-copy">
+            <AddressRules
+              country={address.country}
+              fetch={country =>
+                import('@vtex/address-form/lib/country/' + country)
+              }
+            >
+              <AddressSummary address={address} />
+            </AddressRules>
+          </div>
+        </DataEntry>
+      </div>
+      {/* <div className="lighter black-40 lh-copy pv4 w5 h4">
+        <AddressRules
+          country={address.country}
+          fetch={country => import('@vtex/address-form/lib/country/' + country)}
+        >
+          <AddressSummary address={address} />
+        </AddressRules>
+      </div> */}
+    </ContentBox>
+  )
+}
+
+PaymentBox.propTypes = {
+  intl: intlShape.isRequired,
+}
+
+export default injectIntl(PaymentBox)
--- react/pages/payments/Payments.js
+++ react/pages/payments/Payments.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
+import { compose, branch, renderComponent, withProps } from 'recompose'
 import PaymentsHeader from './PaymentsHeader'
+// import AddressesLoading from './AddressesLoading'
+import PaymentBox from '../../components/Payments/PaymentBox'
+import Toast from '../../components/shared/Toast'
+import GetAddresses from '../../graphql/getAddresses.gql'
 
-const Payments = () => {
-  return (
-    <div>
-      <PaymentsHeader />
-    </div>
-  )
+class Payments extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showToast: false,
+    }
+  }
+
+  componentDidMount() {
+    const { location } = this.props
+    this.setState({ showToast: location.search.indexOf('success=true') > -1 })
+  }
+
+  handleCloseToast = () => {
+    this.setState({ showToast: false })
+  }
+
+  render() {
+    const { addresses } = this.props
+    const { showToast } = this.state
+
+    return (
+      <section>
+        <PaymentsHeader />
+        <main className="mt7 flex-ns flex-wrap-ns items-start-ns relative">
+          {addresses.map(address => (
+            <PaymentBox
+              address={address}
+              onEditClick={() => this.startEditing(address)}
+            />
+          ))}
+          {showToast && (
+            <Toast messageId="alert.success" onClose={this.handleCloseToast} />
+          )}
+        </main>
+      </section>
+    )
+  }
 }
 
-export default Payments
+Payments.propTypes = {
+  addresses: PropTypes.array.isRequired,
+}
+
+const enhance = compose(
+  graphql(GetAddresses),
+  branch(({ data }) => data.profile == null, renderComponent(PaymentsHeader)),
+  withProps(({ data }) => ({ addresses: data.profile.addresses })),
+  withRouter,
+)
+export default enhance(Payments)
