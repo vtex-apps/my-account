ab5b14c974087415e69fac53bed6b8ba44d3e59c
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-28 18:17:06 -0300

    Add set password action

--- react/components/Profile/PasswordFormBox.js
+++ react/components/Profile/PasswordFormBox.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
-import ContentBox from '../shared/ContentBox'
+import { graphql } from 'react-apollo'
+import { compose } from 'recompose'
 import { Input, Button } from 'vtex.styleguide'
+import ContentBox from '../shared/ContentBox'
+import GenericError from '../shared/GenericError'
+import SetPassword from '../../graphql/setPassword.gql'
+import PasswordValidator from './PasswordValidator'
 
 class PasswordFormBox extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      currentPassword: '',
       newPassword: '',
       confirmPassword: '',
+      newPasswordTouched: false,
+      confirmPasswordTouched: false,
+      newPasswordValid: false,
+      changeAttempts: 0,
+      isLoading: false,
+      error: null,
     }
   }
 
@@ -17,48 +29,146 @@ class PasswordFormBox extends Component {
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  handleSubmit = e => {
-    e.preventDefault()
-    // validation and submission logic goes here
-    this.props.onPasswordChange()
+  touchField = e => {
+    this.setState({ [`${e.target.name}Touched`]: true })
+  }
+
+  handleValidationChange = ({ valid }) => {
+    this.setState({ newPasswordValid: valid })
+  }
+
+  handleSubmit = async e => {
+    const { email, onPasswordChange } = this.props
+    const {
+      currentPassword,
+      newPassword,
+      newPasswordValid,
+      confirmPassword,
+      changeAttempts,
+    } = this.state
+    if (newPassword !== confirmPassword || !newPasswordValid) return
+
+    this.setState({
+      isLoading: true,
+      error: null,
+      changeAttempts: changeAttempts + 1,
+    })
+    try {
+      await this.props.setPassword({
+        variables: { email, currentPassword, newPassword },
+      })
+      this.setState({ isLoading: false, changeAttempts: 0 })
+      onPasswordChange()
+    } catch (error) {
+      const wrongPassword = error.toString().indexOf('Wrong credentials') > -1
+      const blockedUser = error.toString().indexOf('Blocked') > -1
+      this.setState(prevState => ({
+        isLoading: false,
+        error:
+          wrongPassword && prevState.changeAttempts === 3
+            ? 'alert.wrongAndAboutToBlock'
+            : wrongPassword
+              ? 'alert.wrongPassword'
+              : blockedUser
+                ? 'alert.blockedUser'
+                : 'alert.unknownError',
+      }))
+    }
+  }
+
+  dismissError = () => {
+    this.setState({ error: null })
   }
 
   render() {
     const { intl } = this.props
-    const { newPassword, confirmPassword } = this.state
+    const {
+      currentPassword,
+      newPassword,
+      confirmPassword,
+      newPasswordTouched,
+      confirmPasswordTouched,
+      newPasswordValid,
+      isLoading,
+      error,
+    } = this.state
+
+    const passwordsTouched = newPasswordTouched && confirmPasswordTouched
+    const passwordMismatch = newPassword !== confirmPassword
+    const shouldEnableSubmit =
+      currentPassword && !passwordMismatch && newPasswordValid
+
     return (
       <ContentBox shouldAllowGrowing>
-        <form onSubmit={this.handleSubmit}>
+        {error && (
           <div className="mb7">
-            <Input
-              name="newPassword"
-              value={newPassword}
-              onChange={this.handleChange}
-              type="password"
-              label={intl.formatMessage({ id: 'personalData.newPassword' })}
-            />
+            <GenericError onDismiss={this.dismissError} errorId={error} />
           </div>
-          <div className="mb7">
-            <Input
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={this.handleChange}
-              type="password"
-              label={intl.formatMessage({ id: 'personalData.confirmPassword' })}
-            />
-          </div>
-          <Button type="submit" variation="secondary" block size="small">
-            {intl.formatMessage({ id: 'personalData.savePassword' })}
-          </Button>
-        </form>
+        )}
+        <div className="mb7">
+          <Input
+            name="currentPassword"
+            value={currentPassword}
+            onChange={this.handleChange}
+            type="password"
+            label={intl.formatMessage({ id: 'personalData.currentPassword' })}
+          />
+        </div>
+        <div className="mb7">
+          <Input
+            name="newPassword"
+            value={newPassword}
+            onChange={this.handleChange}
+            onBlur={this.touchField}
+            type="password"
+            label={intl.formatMessage({ id: 'personalData.newPassword' })}
+          />
+        </div>
+        <div className="mb7">
+          <PasswordValidator
+            password={newPassword}
+            onValidationChange={this.handleValidationChange}
+          />
+        </div>
+        <div className="mb7">
+          <Input
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            onBlur={this.touchField}
+            errorMessage={
+              passwordsTouched && passwordMismatch
+                ? intl.formatMessage({ id: 'alert.passwordMismatch' })
+                : null
+            }
+            type="password"
+            label={intl.formatMessage({ id: 'personalData.confirmPassword' })}
+          />
+        </div>
+        <Button
+          block
+          size="small"
+          onClick={this.handleSubmit}
+          isLoading={isLoading}
+          disabled={!shouldEnableSubmit}
+        >
+          {intl.formatMessage({ id: 'personalData.savePassword' })}
+        </Button>
       </ContentBox>
     )
   }
 }
 
 PasswordFormBox.propTypes = {
+  email: PropTypes.string.isRequired,
+  onPasswordChange: PropTypes.func.isRequired,
+  redefinePassword: PropTypes.func.isRequired,
   intl: intlShape.isRequired,
-  onPasswordChange: PropTypes.func,
 }
 
-export default injectIntl(PasswordFormBox)
+const enhance = compose(
+  graphql(SetPassword, { name: 'setPassword' }),
+  injectIntl,
+)
+
+export default enhance(PasswordFormBox)
