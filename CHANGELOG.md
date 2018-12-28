534ee77f298e7b122dd6c798db2e107d08e9d29e
Bruno Dias <bruno.dias@vtex.com.br>
2018-12-28 13:22:49 -0200

    Add store and messages builder

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- New builders are in the town! Add messages and store builders.
 
 ## [0.19.6] - 2018-12-26
-
 ### Changed
 - Use Skeleton components provided by `vtex.my-account-commons`
 
--- react/components/Profile/ProfileBox.js
+++ react/components/Profile/ProfileBox.js
@@ -37,91 +37,91 @@ const ProfileBox = ({ profile, storeCountry, onEditClick, intl }) => {
             },
             isCorporate,
           }) => (
-            <React.Fragment>
-              <div>
-                <div className="flex-ns flex-wrap">
-                  <div className="mb8 flex-auto">
-                    <DataEntry label={firstName.label}>
-                      {firstName.value}
-                    </DataEntry>
-                  </div>
-                  <div className="mb8 flex-auto">
-                    <DataEntry label={lastName.label}>
-                      {lastName.value}
-                    </DataEntry>
-                  </div>
-                </div>
-                <div className="mb8">
-                  <DataEntry label={email.label}>{email.value}</DataEntry>
-                </div>
-                <div className="flex-ns flex-wrap">
-                  {document && document.label && (
-                    <div className="mb8 flex-auto">
-                      <DataEntry label={document.label}>
-                        {document.value}
-                      </DataEntry>
-                    </div>
-                  )}
-                  <div className="mb8 w-50-ns">
-                    <DataEntry label={gender.label}>{gender.value}</DataEntry>
-                  </div>
-                </div>
-                <div className="flex-ns flex-wrap">
-                  <div className="mb8 flex-auto">
-                    <DataEntry label={birthDate.label}>
-                      {birthDate.value}
-                    </DataEntry>
-                  </div>
-                  <div className="mb8 w-50-ns">
-                    <DataEntry label={homePhone.label}>
-                      {homePhone.value}
-                    </DataEntry>
-                  </div>
-                </div>
-              </div>
-              {isCorporate && (
+              <React.Fragment>
                 <div>
-                  <div className="mb8">
-                    <DataEntry label={corporateName.label}>
-                      {corporateName.value}
-                    </DataEntry>
-                  </div>
-                  {tradeName && tradeName.label && (
-                    <div className="mb8">
-                      <DataEntry label={tradeName.label}>
-                        {tradeName.value}
-                      </DataEntry>
-                    </div>
-                  )}
-                  {corporateDocument && corporateDocument.label && (
-                    <div className="mb8">
-                      <DataEntry label={corporateDocument.label}>
-                        {corporateDocument.value}
-                      </DataEntry>
-                    </div>
-                  )}
                   <div className="flex-ns flex-wrap">
                     <div className="mb8 flex-auto">
-                      <DataEntry label={businessPhone.label}>
-                        {businessPhone.value}
+                      <DataEntry label={firstName.label}>
+                        {firstName.value}
                       </DataEntry>
                     </div>
-                    {stateRegistration && stateRegistration.label && (
-                      <div className="mb8 w-50-ns">
-                        <DataEntry label={stateRegistration.label}>
-                          {stateRegistration.value}
+                    <div className="mb8 flex-auto">
+                      <DataEntry label={lastName.label}>
+                        {lastName.value}
+                      </DataEntry>
+                    </div>
+                  </div>
+                  <div className="mb8">
+                    <DataEntry label={email.label}>{email.value}</DataEntry>
+                  </div>
+                  <div className="flex-ns flex-wrap">
+                    {document && document.label && (
+                      <div className="mb8 flex-auto">
+                        <DataEntry label={document.label}>
+                          {document.value}
                         </DataEntry>
                       </div>
                     )}
+                    <div className="mb8 w-50-ns">
+                      <DataEntry label={gender.label}>{gender.value}</DataEntry>
+                    </div>
+                  </div>
+                  <div className="flex-ns flex-wrap">
+                    <div className="mb8 flex-auto">
+                      <DataEntry label={birthDate.label}>
+                        {birthDate.value}
+                      </DataEntry>
+                    </div>
+                    <div className="mb8 w-50-ns">
+                      <DataEntry label={homePhone.label}>
+                        {homePhone.value}
+                      </DataEntry>
+                    </div>
                   </div>
                 </div>
-              )}
-            </React.Fragment>
-          )}
+                {isCorporate && (
+                  <div>
+                    <div className="mb8">
+                      <DataEntry label={corporateName.label}>
+                        {corporateName.value}
+                      </DataEntry>
+                    </div>
+                    {tradeName && tradeName.label && (
+                      <div className="mb8">
+                        <DataEntry label={tradeName.label}>
+                          {tradeName.value}
+                        </DataEntry>
+                      </div>
+                    )}
+                    {corporateDocument && corporateDocument.label && (
+                      <div className="mb8">
+                        <DataEntry label={corporateDocument.label}>
+                          {corporateDocument.value}
+                        </DataEntry>
+                      </div>
+                    )}
+                    <div className="flex-ns flex-wrap">
+                      <div className="mb8 flex-auto">
+                        <DataEntry label={businessPhone.label}>
+                          {businessPhone.value}
+                        </DataEntry>
+                      </div>
+                      {stateRegistration && stateRegistration.label && (
+                        <div className="mb8 w-50-ns">
+                          <DataEntry label={stateRegistration.label}>
+                            {stateRegistration.value}
+                          </DataEntry>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </React.Fragment>
+            )}
         </ProfileSummary>
       </ProfileRules>
       <ExtensionPoint
-        id="profile/display"
+        id="profile-display"
         render={fields => (
           <div className="flex-ns flex-wrap">
             {fields.map(({ label, value }) => (
