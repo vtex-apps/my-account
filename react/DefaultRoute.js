0822298fce10d8dfde9454731bddec88b4c04b49
Bruno Dias <bruno.dias@vtex.com.br>
2018-12-28 09:33:21 -0200

    Update to react 3 and messages 1
    Release v1.3.2
    
    Changing tests defaultLocales and messages location
    
    Release v1.4.1
    
    Delete node/ directory

--- react/DefaultRoute.js
+++ react/DefaultRoute.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { ExtensionPoint } from 'render'
 import { Link, withRouter } from 'vtex.my-account-commons/Router'
 
 class DefaultRoute extends Component {
--- react/Menu.js
+++ react/Menu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { ExtensionPoint } from 'render'
 import { Link, withRouter } from 'vtex.my-account-commons/Router'
+import { ExtensionPoint } from 'vtex.render-runtime'
 
 class Menu extends Component {
   componentDidMount() {
