7804eebe1788ea80cc2017ecdc4f71e95120336a
Bruno Dias <bruno.dias@vtex.com.br>
2018-12-28 09:33:21 -0200

    Update to react 3 and messages 1

--- react/components/Profile/PasswordBox.js
+++ react/components/Profile/PasswordBox.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { injectIntl, intlShape } from 'react-intl'
+
 import ContentBox from '../shared/ContentBox'
 import DataEntry from '../shared/DataEntry'
+
+import styles from '../../styles.css'
 
 const PasswordBox = ({ onEditClick, intl }) => {
   return (
@@ -10,7 +13,7 @@ const PasswordBox = ({ onEditClick, intl }) => {
       shouldAllowGrowing
       lowerButton={intl.formatMessage({ id: 'personalData.redefinePassword' })}
       onLowerButtonClick={onEditClick}>
-      <div className="vtex-account__password-box w-100">
+      <div className={`${styles.passwordBox} w-100`}>
         <DataEntry label={intl.formatMessage({ id: 'personalData.password' })}>
           *******************
         </DataEntry>
