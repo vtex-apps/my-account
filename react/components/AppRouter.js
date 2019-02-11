140073e00a5be8258fca6b30e4ecd98c5d73ffc4
Breno Calazans <brenocalazans@gmail.com>
2019-02-11 15:30:53 -0200

    Add possibility of changing the Menu and setting the home page of My Account

--- react/components/AppRouter.js
+++ react/components/AppRouter.js
@@ -1,66 +1,94 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Route, Switch, Redirect, HashRouter } from 'react-router-dom'
 import Media from 'react-media'
 import { ExtensionPoint } from 'render'
-import Menu from './Menu/Menu'
+import Menu from './Menu'
 import Addresses from './pages/Addresses'
 import Profile from './pages/Profile'
 import ProfileEdit from './pages/ProfileEdit'
 import AddressCreate from './pages/AddressCreate'
 import AddressEdit from './pages/AddressEdit'
 
-const AppRouter = () => {
-  const routes = [
-    { path: '/addresses', component: Addresses },
-    { path: '/addresses/new', component: AddressCreate },
-    { path: '/addresses/edit/:id', component: AddressEdit },
-    { path: '/profile', component: Profile },
-    { path: '/profile/edit', component: ProfileEdit },
-  ]
+class AppRouter extends Component {
+  state = { defaultPath: null }
 
-  // eslint-disable-next-line
-  const toRouteComponent = ({ path, component }) => (
-    <Route exact key={path} path={path} component={component} />
-  )
+  handleDefaultPath = (path) => {
+    this.setState({ defaultPath: path })
+  }
 
-  // eslint-disable-next-line
-  const shouldRedirectOrder = vtex && vtex.orderListRendered
+  render() {
+    const routes = [
+      { path: '/addresses', component: Addresses },
+      { path: '/addresses/new', component: AddressCreate },
+      { path: '/addresses/edit/:id', component: AddressEdit },
+      { path: '/profile', component: Profile },
+      { path: '/profile/edit', component: ProfileEdit },
+    ]
 
-  return (
-    <div className="w-100 mw9 pv7-m pv9-l flex">
-      <HashRouter>
-        <Media query="(max-width: 45em)">
-          {matches =>
-            matches ? (
-              <Switch>
-                <Route exact path="/" component={Menu} />
-                {routes.map(toRouteComponent)}
-                <Redirect
-                  exact
-                  from="/"
-                  to={shouldRedirectOrder ? '/orders' : '/profile'}
-                />
-                <ExtensionPoint id="routes" />
-              </Switch>
-            ) : (
-              <Fragment>
-                <Menu />
-                <Switch>
-                  {routes.map(toRouteComponent)}
-                  <Redirect
-                    exact
-                    from="/"
-                    to={shouldRedirectOrder ? '/orders' : '/profile'}
-                  />
-                  <ExtensionPoint id="routes" />
-                </Switch>
-              </Fragment>
-            )
-          }
-        </Media>
-      </HashRouter>
-    </div>
-  )
+    // eslint-disable-next-line
+    const toRouteComponent = ({ path, component }) => (
+      <Route exact key={path} path={path} component={component} />
+    )
+
+    // eslint-disable-next-line
+    const shouldRedirectOrder = vtex && vtex.orderListRendered
+
+    return (
+      <div className="w-100 mw9 pv7-m pv9-l flex">
+        <ExtensionPoint
+          id="defaultRoute"
+          onSetDefaultPath={this.handleDefaultPath} />
+        {this.state.defaultPath && (
+          <HashRouter>
+            <Media query="(max-width: 45em)">
+              {matches =>
+                matches ? (
+                  <Switch>
+                    <Route exact path="/" component={Menu} />
+                    {routes.map(toRouteComponent)}
+                    <Redirect
+                      exact
+                      from="/"
+                      to={shouldRedirectOrder ? '/orders' : this.state.defaultPath}
+                    />
+                    <ExtensionPoint id="routes" />
+                  </Switch>
+                ) : (
+                  <Fragment>
+                    <Menu />
+                    <Switch>
+                      {routes.map(toRouteComponent)}
+                      <Redirect
+                        exact
+                        from="/"
+                        to={shouldRedirectOrder ? '/orders' : this.state.defaultPath}
+                      />
+                      <ExtensionPoint id="routes" />
+                    </Switch>
+                  </Fragment>
+                )
+              }
+            </Media>
+          </HashRouter>
+        )}
+      </div>
+    )
+  }
+}
+
+AppRouter.getSchema = () => {
+  return {
+    title: 'My Account Home',
+    description: 'Home',
+    type: 'object',
+    properties: {
+      defaultPath: {
+        title: 'Default path',
+        type: 'string',
+        default: '/profile',
+      }
+    }
+  }
 }
 
 export default AppRouter
