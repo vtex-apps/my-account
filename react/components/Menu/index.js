fb425f0fe1c7a039a14881041de469ad784f19f0
Breno Calazans <brenocalazans@gmail.com>
2019-02-11 16:08:02 -0200

    Simplify condition

--- react/components/Menu/index.js
+++ react/components/Menu/index.js
@@ -19,7 +19,7 @@ class Menu extends Component {
       <aside className="vtex-account__menu pv9 pv0-m-2 ph9 ph7-m ph8-l w-20-m w-100">
         <UserInfo />
         <ExtensionPoint id="menu" hasExtension={this.handleExtension} />
-        {hasExtension !== null && hasExtension === false && <MenuLinksList />}
+        {hasExtension === false && <MenuLinksList />}
       </aside>
     )
   }
