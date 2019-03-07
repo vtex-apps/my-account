b925c813093540b644754fe5ed13e1fb8df4483c
Felipe Sales <sales.felipeb@gmail.com>
2019-03-07 15:20:22 -0300

    Adding test config

---
+++ react/testUtils/index.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from 'react'
+import { IntlProvider } from 'react-intl'
+import * as reactTestingLibrary from 'react-testing-library'
+
+import defaultStrings from '../locales/en.json'
+
+const customRender = (node: ReactNode, options?: any) => {
+  const rendered = reactTestingLibrary.render(
+    <IntlProvider messages={defaultStrings} locale="en-US">
+      {node}
+    </IntlProvider>,
+    options
+  )
+
+  return {
+    ...rendered,
+    rerender: (newUi: any) =>
+      customRender(newUi, {
+        baseElement: rendered.baseElement,
+        container: rendered.container,
+      }),
+  }
+}
+
+// re-export everything
+export const render = customRender
+export default {
+  ...reactTestingLibrary,
+}
