dcdc62a525728ee0a42dbac9309f61def77e86cc
arthurepc <arthurcosta@vtex.com.br>
2019-04-17 16:52:10 -0300

    Add logout in my account

--- react/components/Menu/MenuLinksList.js
+++ react/components/Menu/MenuLinksList.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { injectIntl, intlShape } from 'react-intl'
+import React, { Component, Fragment } from 'react'
+import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
 import { ExtensionPoint } from 'render'
+import { AuthService } from 'vtex.react-vtexid'
+import { ModalDialog } from 'vtex.styleguide'
 import MenuLink from './MenuLink'
 
 const links = [
@@ -14,30 +16,71 @@ const links = [
   },
 ]
 
-const MenuLinksList = ({ intl }) => {
-  return (
-    <nav className="vtex-account__menu-links">
-      <ExtensionPoint
-        id="menu-links-before"
-        render={links =>
-          links.map(({ name, path }) => (
-            <MenuLink path={path} name={name} key={name} />
-          ))
-        }
-      />
-      {links.map(({ path, id }) => (
-        <MenuLink path={path} name={intl.formatMessage({ id })} key={id} />
-      ))}
-      <ExtensionPoint
-        id="menu-links-after"
-        render={links =>
-          links.map(({ name, path }) => (
-            <MenuLink path={path} name={name} key={name} />
-          ))
-        }
-      />
-    </nav>
-  )
+class MenuLinksList extends Component {
+
+  state = { isModalOpen: false }
+
+  handleModalToggle = () => {
+    console.log(this)
+    this.setState(prevState => ({ isModalOpen: !prevState.isModalOpen }))
+  }
+
+  render() {
+    const { intl } = this.props
+
+    return (
+      <nav className="vtex-account__menu-links">
+        <ExtensionPoint
+          id="menu-links-before"
+          render={links =>
+            links.map(({ name, path }) => (
+              <MenuLink path={path} name={name} key={name} />
+            ))
+          }
+        />
+        {links.map(({ path, id }) => (
+          <MenuLink path={path} name={intl.formatMessage({ id })} key={id} />
+        ))}
+        <ExtensionPoint
+          id="menu-links-after"
+          render={links =>
+            links.map(({ name, path }) => (
+              <MenuLink path={path} name={name} key={name} />
+            ))
+          }
+        />
+        <AuthService.RedirectLogout returnUrl="/">
+            {({ action: logout }) => (
+              <Fragment>
+                <div
+                  className={`vtex-account_menu-link f6 no-underline db hover-near-black pv5 mv3 pl5 bl bw2 nowrap c-muted-1 b--transparent pointer`}
+                  onClick={this.handleModalToggle}>
+                  <FormattedMessage id="pages.logout" />
+                </div>
+
+                <ModalDialog
+                  centered
+                  confirmation={{
+                    onClick: logout,
+                    label: intl.formatMessage({ id: "pages.logout" }),
+                  }}
+                  cancelation={{
+                    onClick: this.handleModalToggle,
+                    label: intl.formatMessage({ id: "logoutModal.cancel" }),
+                  }}
+                  isOpen={this.state.isModalOpen}
+                  onClose={this.handleModalToggle}
+                  >
+                  <h1>
+                    <FormattedMessage id="logoutModal.title" />
+                  </h1>
+                </ModalDialog>
+              </Fragment>
+            )}
+        </AuthService.RedirectLogout>
+      </nav>
+    )
+  }
 }
 
 MenuLinksList.propTypes = {
