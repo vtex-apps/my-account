c8cb2f88ee472c062021138b647eeb41416f2962
Christian Kaisermann <christian@kaisermann.me>
2019-09-17 17:27:49 -0300

    Allow to edit an address using geolocation

--- react/components/Addresses/AddressForm.tsx
+++ react/components/Addresses/AddressForm.tsx
@@ -42,19 +42,42 @@ interface State {
   address: any
 }
 
+const AUTO_COMPLETABLE_FIELDS = [
+  'city',
+  'geoCoordinates',
+  'neighborhood',
+  'number',
+  'postalCode',
+  'state',
+  'street',
+]
 class AddressForm extends Component<InnerProps & OuterProps, State> {
   public constructor(props: InnerProps & OuterProps) {
     super(props)
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { __typename, addressName, ...addr } = props.address
+    let { __typename, addressName, ...addressValues } = props.address
 
-    this.state = {
-      address: addValidation({
-        addressQuery: null,
-        ...addr,
-      }),
+    const address = addValidation({
+      addressQuery: null,
+      ...addressValues,
+    })
+
+    /**
+     * If geo coordinates are available, we assume everything is valid and was autocompleted
+     * by the geolocation or postalcode components.
+     * */
+    if (
+      addressValues.geoCoordinates &&
+      addressValues.geoCoordinates.length === 2
+    ) {
+      AUTO_COMPLETABLE_FIELDS.forEach(field => {
+        address[field].geolocationAutoCompleted = true
+        address[field].postalCodeAutoCompleted = true
+        address[field].valid = true
+      })
     }
+
+    this.state = { address }
   }
 
   private handleSubmit = (isValid: boolean, address: Address) => {
@@ -65,35 +88,54 @@ class AddressForm extends Component<InnerProps & OuterProps, State> {
     }
   }
 
-  private handleAddressChange = (newAddress: Address) => {
-    const address = {
-      ...this.state.address,
-      ...newAddress,
+  private handleAddressChange = (newAddress: any) => {
+    const curAddress = this.state.address
+
+    // allow to edit only with geolocation component
+    if (
+      this.hasGeolocationPreference() &&
+      curAddress.postalCode &&
+      curAddress.postalCode.geolocationAutoCompleted &&
+      newAddress.postalCode &&
+      !newAddress.postalCode.geolocationAutoCompleted
+    ) {
+      AUTO_COMPLETABLE_FIELDS.forEach(field => {
+        newAddress[field].value = null
+        newAddress[field].valid = false
+      })
     }
 
+    const address = { ...curAddress, ...newAddress }
     this.setState({ address })
   }
 
-  private hasValidGeoCoords() {
+  private hasGeoCoords() {
     const { address } = this.state
     return (
       address.geoCoordinates &&
-      address.geoCoordinates.valid &&
-      address.geoCoordinates.geolocationAutoCompleted
+      Array.isArray(address.geoCoordinates.value) &&
+      address.geoCoordinates.value.length === 2
     )
   }
 
   private hasValidPostalCode() {
     const { address } = this.state
-    return address.postalCode.value !== null
+    return address.postalCode.valid === true
   }
 
   private hasAutoCompletedFields() {
     const { address } = this.state
-    return Object.keys(address).some(
+    return AUTO_COMPLETABLE_FIELDS.some(
       fieldName =>
         (address && address[fieldName].geolocationAutoCompleted) ||
         (address && address[fieldName].postalCodeAutoCompleted)
+    )
+  }
+
+  private hasGeolocationPreference() {
+    const { getStoreConfigs } = this.props
+    return (
+      getStoreConfigs.storeConfigs && getStoreConfigs.storeConfigs.geolocation
     )
   }
 
@@ -105,99 +147,96 @@ class AddressForm extends Component<InnerProps & OuterProps, State> {
     }))
   }
 
-  public render() {
-    const { address } = this.state
-    const { intl, submitLabelId, isLoading, getStoreConfigs } = this.props
-
-    const shipCountries = this.getLocalizedShipsTo()
-    const hasValidGeoCoords = this.hasValidGeoCoords()
-    const hasValidPostalCode = this.hasValidPostalCode()
-    const hasAutoCompletedFields = this.hasAutoCompletedFields()
-
-    const mapsAPIKey =
+  private getMapsAPIKey() {
+    const { getStoreConfigs } = this.props
+    return (
       getStoreConfigs.storeConfigs &&
       getStoreConfigs.storeConfigs.googleMapsApiKey
+    )
+  }
 
-    const isGeolocation =
-      getStoreConfigs.storeConfigs && getStoreConfigs.storeConfigs.geolocation
+  public render() {
+    const { address } = this.state
+    const { intl, submitLabelId, isLoading } = this.props
+
+    const shipCountries = this.getLocalizedShipsTo()
+    const hasGeoCoords = this.hasGeoCoords()
+    const hasValidPostalCode = this.hasValidPostalCode()
+    const hasAutoCompletedFields = this.hasAutoCompletedFields()
+    const mapsAPIKey = this.getMapsAPIKey()
+    const prefersGeolocation = this.hasGeolocationPreference()
 
     return (
-      <Fragment>
-        <AddressRules country={address.country.value} shouldUseIOFetching>
-          <AddressContainer
-            address={address}
-            Input={StyleguideInput}
-            onChangeAddress={this.handleAddressChange}
-            autoCompletePostalCode>
-            <Fragment>
-              <CountrySelector shipsTo={shipCountries} />
+      <AddressRules country={address.country.value} shouldUseIOFetching>
+        <AddressContainer
+          address={address}
+          Input={StyleguideInput}
+          onChangeAddress={this.handleAddressChange}
+          autoCompletePostalCode>
+          <Fragment>
+            <CountrySelector shipsTo={shipCountries} />
 
-              {isGeolocation && (
-                <GoogleMapsContainer apiKey={mapsAPIKey} locale={intl.locale}>
-                  {({
-                    loading,
-                    googleMaps,
-                  }: {
-                    loading: boolean
-                    googleMaps: string
-                  }) => (
-                    <Fragment>
+            {prefersGeolocation === true && (
+              <GoogleMapsContainer apiKey={mapsAPIKey} locale={intl.locale}>
+                {({ loading, googleMaps }: GoogleMapsContainerArgs) => (
+                  <Fragment>
+                    {!hasAutoCompletedFields && (
                       <GeolocationInput
                         loadingGoogle={loading}
                         googleMaps={googleMaps}
                       />
+                    )}
 
-                      {hasValidGeoCoords && (
-                        <Map
-                          loadingGoogle={loading}
-                          googleMaps={googleMaps}
-                          mapProps={{ className: 'mb7 br2 h4' }}
-                        />
-                      )}
-                    </Fragment>
-                  )}
-                </GoogleMapsContainer>
-              )}
-
-              {isGeolocation === false && <PostalCodeGetter />}
-
-              {hasAutoCompletedFields && (
-                <div className="pb7">
-                  <AutoCompletedFields>
-                    <span className="c-link pointer">
-                      <FormattedMessage id="address-form.edit" />
-                    </span>
-                  </AutoCompletedFields>
-                </div>
-              )}
-
-              {(hasValidGeoCoords || hasValidPostalCode) && (
-                <AddressFields
-                  address={address}
-                  Input={StyleguideInput}
-                  onChangeAddress={this.handleAddressChange}
-                  notApplicableLabel={intl.formatMessage({
-                    id: 'addresses.notApplicable',
-                  })}
-                />
-              )}
-
-              <AddressSubmitter onSubmit={this.handleSubmit}>
-                {(handleSubmit: () => void) => (
-                  <Button
-                    onClick={handleSubmit}
-                    block
-                    size="small"
-                    isLoading={isLoading}
-                    disabled={!hasValidGeoCoords && !hasValidPostalCode}>
-                    <FormattedMessage id={submitLabelId} />
-                  </Button>
+                    {hasGeoCoords && (
+                      <Map
+                        loadingGoogle={loading}
+                        googleMaps={googleMaps}
+                        mapProps={{ className: 'mb7 br2 h4' }}
+                      />
+                    )}
+                  </Fragment>
                 )}
-              </AddressSubmitter>
-            </Fragment>
-          </AddressContainer>
-        </AddressRules>
-      </Fragment>
+              </GoogleMapsContainer>
+            )}
+
+            {prefersGeolocation === false && <PostalCodeGetter />}
+
+            {hasAutoCompletedFields && (
+              <div className="mb7">
+                <AutoCompletedFields>
+                  <span className="c-link pointer">
+                    <FormattedMessage id="address-form.edit" />
+                  </span>
+                </AutoCompletedFields>
+              </div>
+            )}
+
+            {(hasGeoCoords || hasValidPostalCode) && (
+              <AddressFields
+                address={address}
+                Input={StyleguideInput}
+                onChangeAddress={this.handleAddressChange}
+                notApplicableLabel={intl.formatMessage({
+                  id: 'addresses.notApplicable',
+                })}
+              />
+            )}
+
+            <AddressSubmitter onSubmit={this.handleSubmit}>
+              {(handleSubmit: () => void) => (
+                <Button
+                  onClick={handleSubmit}
+                  block
+                  size="small"
+                  isLoading={isLoading}
+                  disabled={!hasGeoCoords && !hasValidPostalCode}>
+                  <FormattedMessage id={submitLabelId} />
+                </Button>
+              )}
+            </AddressSubmitter>
+          </Fragment>
+        </AddressContainer>
+      </AddressRules>
     )
   }
 }
