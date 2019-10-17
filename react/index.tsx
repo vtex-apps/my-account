f208d216fc71774d0cb0d1258857a8f6c92908e9
Felipe Sales <sales.felipeb@gmail.com>
2019-10-17 14:37:41 -0300

    Remove url logging

--- react/index.tsx
+++ react/index.tsx
@@ -3,21 +3,10 @@ import 'vtex.country-codes/locales'
 
 import AppRouter from './components/AppRouter'
 import Wrapper from './components/MyAccountWrapper'
-import { logMyAccountURL, logGeneralErrors } from './utils/splunk'
+import { logGeneralErrors } from './utils/splunk'
 
 class MyAccount extends Component {
-  public constructor(props: any) {
-    super(props)
-
-    if (
-      window.location.href.match('/account/orders') &&
-      window.__RUNTIME__.workspace === 'master'
-    ) {
-      logMyAccountURL()
-    }
-  }
-
-  public componentDidCatch(error: any, info: any) {
+  public componentDidCatch(error: unknown, info: unknown) {
     if (window.__RUNTIME__.workspace === 'master') {
       logGeneralErrors(error, info)
     }
--- react/utils/splunk.ts
+++ react/utils/splunk.ts
@@ -7,14 +7,7 @@ splunkEvents.config({
   token: 'feee5906-39b2-43a5-bb46-b927b8e01da3',
 })
 
-export function logMyAccountURL() {
-  const { account } = window.__RUNTIME__
-  splunkEvents.logEvent('Important', 'Info', 'MyAccount', 'MyAccountURL', {
-    account: account,
-  })
-}
-
-export function logGeneralErrors(error: any, info: any) {
+export function logGeneralErrors(error: unknown, info: unknown) {
   const { account } = window.__RUNTIME__
   splunkEvents.logEvent(
     'Critical',
