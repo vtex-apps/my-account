4d3767aa198e92a38fb6b853f7bd455abfefb041
Felipe Sales <sales.felipeb@gmail.com>
2021-03-09 16:01:37 -0300

    Remove unnused routes

--- node/index.ts
+++ node/index.ts
@@ -22,13 +22,5 @@ export default new Service<IOClients, RecorderState, Context>({
         cacheId: () => process.env.VTEX_APP_ID,
       },
     },
-  },
-  /* This is necessary for sustaining compatibility with the environment `vtexcommerce`.
-  It checks if the app is installed or not through this endpoint. DO NOT REMOVE IT! */
-  routes: {
-    enabled: (ctx: Context) => {
-      ctx.response.status = 204
-      ctx.response.body = 'My Account app is installed in this Store.'
-    },
-  },
+  }
 })
