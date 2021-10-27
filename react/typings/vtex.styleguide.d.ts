73436856c1a79f64fc38e29f33854871aacbeff5
Rafael Rubim Cabral <rafael.cabral@vtex.com.br>
2021-10-27 16:04:24 -0300

    Remove unused typings

--- react/typings/vtex.styleguide.d.ts
+++ react/typings/vtex.styleguide.d.ts
@@ -2,10 +2,6 @@ declare module 'vtex.styleguide' {
   export const Button
   export const EmptyState
   export const Alert
-  export const InputPassword
-  export const Input
-  export const IconSuccess
-  export const IconFailure
   export const Modal
   export const Spinner
   export const ModalDialog
